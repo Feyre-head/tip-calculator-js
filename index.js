72bba03d92368c8aed029af33a5bc2a319695b2b
Inara Oenning <inaraoenning@gmail.com>
2025-07-25 16:24:15 -0300

    Adicionando lógica para retornar Bill Each: Person/People

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ function update() {
     let bill = Number(document.getElementById('yourBill').value)
     let tipPercent = document.getElementById('tipInput').value
     let split = document.getElementById('splitInput').value
+ 
 
     let tipValue = bill * (tipPercent / 100)
     let billTotal = bill + tipValue
     let billEach = billTotal / split
-
-
-    document.getElementById('tipPercent').innerHTML = tipPercent + ' %';
+    let billEachPerson = tipPercent / split
+    
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %'; 
     document.getElementById('tipValue').innerHTML = '$ ' + tipValue;
-    document.getElementById('splitValue').innerHTML = split
     document.getElementById('billEach').innerHTML = '$ ' + billEach
-}
+    document.getElementById('tipValueEach').innerHTML = '$ ' + billEachPerson
+     
 
+    if(split == 1){
+        document.getElementById('splitValue').innerHTML = split + " Person"
+    } else {
+        document.getElementById('splitValue').innerHTML =  split + " People"}
+
+    
+}
 
